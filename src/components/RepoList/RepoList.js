e8467e7d798ed4431d1eb23d8fe40e47d3a3dfc1
Mahmoud <mmhafez51@gmail.com>
2021-08-19 15:41:46 +0200

    optimize rendering by using React.memo(RepoList)

--- src/components/RepoList/RepoList.js
+++ src/components/RepoList/RepoList.js
@@ -1,22 +1,24 @@
 import React from "react";
 import Repo from "../Repo/Repo";
 
-const RepoList = (props) => (
-  <div>
-    {props.repos.map((repo) => (
-      <Repo
-        key={repo.id}
-        avatarUrl={repo.owner.avatar_url}
-        title={repo.name}
-        desc={repo.description}
-        owner={repo.owner.login}
-        date={repo.pushed_at}
-        stars={repo.stargazers_count}
-        issues={repo.open_issues_count}
-        ownerUrl={repo.owner.html_url}
-      />
-    ))}
-  </div>
-);
+const RepoList = (props) => {
+  return (
+    <div>
+      {props.repos.map((repo) => (
+        <Repo
+          key={repo.id}
+          avatarUrl={repo.owner.avatar_url}
+          title={repo.name}
+          desc={repo.description}
+          owner={repo.owner.login}
+          date={repo.pushed_at}
+          stars={repo.stargazers_count}
+          issues={repo.open_issues_count}
+          ownerUrl={repo.owner.html_url}
+        />
+      ))}
+    </div>
+  );
+};
 
-export default RepoList;
+export default React.memo(RepoList);
